5c7b724b560ae5662cb8486e5edc8b496c418f11
ARJUN THAKOR <84375918+The-Arjun-Thakor@users.noreply.github.com>
2022-10-01 19:39:28 +0530

    Add files via upload

---
+++ scripts/hello_world_Example.py
@@ -0,0 +1,9 @@
+# LANGUAGE: Python
+# ENV: Python3.5
+# AUTHOR: Example
+# GITHUB: https://github.com/The-Arjun-Thakor/HacktoberFest-2022
+
+def hello_world():
+	print('Hello, World!')
+
+hello_world()
