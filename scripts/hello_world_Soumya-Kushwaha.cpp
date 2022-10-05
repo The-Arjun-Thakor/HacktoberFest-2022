57eecdfdfdde1bfbff9beffacbaf82f3ac8b1aff
Soumya-Kushwaha <kushsoumya30@Gmail.com>
2022-10-05 20:15:40 +0530

    Soumya

---
+++ scripts/hello_world_Soumya-Kushwaha.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// AUTHOR: Soumya Kushwaha
+// GITHUB: https://github.com/Soumya-Kushwaha
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+
+    return 0;
+}
